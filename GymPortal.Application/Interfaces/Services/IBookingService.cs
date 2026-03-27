ff793417096c12ae8abf717823a795fb9b1a7d60
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-03-27 08:35:34 +0100

    feat(application): add IBookingService interface
    Introduce IBookingService with async methods for booking classes, retrieving user bookings (with and without class details), fetching bookings by session ID, and canceling bookings to support robust booking management functionality.

---
+++ GymPortal.Application/Interfaces/Services/IBookingService.cs
@@ -0,0 +1,15 @@
+﻿using GymPortal.Domain.Common;
+using GymPortal.Domain.Entities;
+
+
+namespace GymPortal.Application.Interfaces.Services
+{
+    public interface IBookingService
+    {
+        Task<Result<Booking>> BookClassAsync(string userId, int sessionId);
+        Task<IEnumerable<Booking>> GetUserBookingsAsync(string userId);
+        Task<IEnumerable<Booking>> GetUserBookingsWithClassAsync(string userId);
+        Task<IEnumerable<Booking>> GetBookingsBySessionIdAsync(int sessionId);
+        Task<Result> CancelBookingAsync(int bookingId);
+    }
+}
