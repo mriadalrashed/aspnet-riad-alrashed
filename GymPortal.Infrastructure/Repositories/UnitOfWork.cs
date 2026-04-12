30850b644261691f2dde33a3485dbe44f4a61264
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-12 19:11:32 +0200

    feat(infrastructure): add UnitOfWork implementation
    Introduced UnitOfWork class in the infrastructure layer to manage repository instances and database transactions. Provides generic repository access, async save changes, and context disposal, improving data access consistency and resource management.

---
+++ GymPortal.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using GymPortal.Application.Interfaces.Repositories;
+using GymPortal.Domain.Common;
+using GymPortal.Infrastructure.Data;
+using System.Collections;
+
+
+namespace GymPortal.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private Hashtable _repositories;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IBaseRepository<T> Repository<T>() where T : BaseEntity
+        {
+            _repositories ??= new Hashtable();
+            var type = typeof(T).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(BaseRepository<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), _context);
+                _repositories.Add(type, repositoryInstance);
+            }
+            return (IBaseRepository<T>)_repositories[type];
+        }
+        public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
