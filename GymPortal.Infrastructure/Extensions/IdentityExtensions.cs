ae82fab949795d8128f55e79bbbc218df0cdd9dd
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-12 19:10:29 +0200

    feat(infrastructure): add IdentityExtensions for Identity setup
    Introduced IdentityExtensions with AddInfrastructureIdentity method to configure ASP.NET Core Identity. Sets up password, lockout, and user options, registers Entity Framework stores with AppDbContext, and adds default token providers.

---
+++ GymPortal.Infrastructure/Extensions/IdentityExtensions.cs
@@ -0,0 +1,32 @@
+﻿using GymPortal.Domain.Entities;
+using GymPortal.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace GymPortal.Infrastructure.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection AddInfrastructureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+            return services;
+        }
+    }
+}
