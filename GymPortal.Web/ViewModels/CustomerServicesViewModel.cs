8a5d41b695ab3e73e9ccbb7166e2cc31c80a5000
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-14 02:32:21 +0200

    feat(web): add CustomerServicesViewModel with ContactForm
    Created CustomerServicesViewModel in the web layer.
    This view model includes a ContactForm property of type ContactFormDto,
    initialized by default, to support customer service interactions.

---
+++ GymPortal.Web/ViewModels/CustomerServicesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using GymPortal.Application.DTOs;
+
+namespace GymPortal.Web.ViewModels
+{
+    public class CustomerServicesViewModel
+    {
+        public ContactFormDto ContactForm { get; set; } = new();
+    }
+}
