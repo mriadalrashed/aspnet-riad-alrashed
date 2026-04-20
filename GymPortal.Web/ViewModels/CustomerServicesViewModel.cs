a824b489cb8edce9963b240d53fdb068d8b88736
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-20 19:00:56 +0200

    refactor(web): remove CustomerServicesViewModel class
    Removed the CustomerServicesViewModel.cs file, including its using directive and class definition, as it is no longer needed in the web layer.

--- GymPortal.Web/ViewModels/CustomerServicesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using GymPortal.Application.DTOs;
-
-namespace GymPortal.Web.ViewModels
-{
-    public class CustomerServicesViewModel
-    {
-        public ContactFormDto ContactForm { get; set; } = new();
-    }
-}
