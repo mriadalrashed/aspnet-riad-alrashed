35491874c33a3e82e24fff5e3c8262bc8cbd6f0d
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-28 17:53:10 +0200

    test(tests): add unit tests for domain exceptions
    Added DomainExceptionTests to verify custom exception messages for DuplicateBookingException and ClassFullException, and to assert that DomainException is abstract. Utilizes FluentAssertions and xUnit for testing.

---
+++ GymPortal.Tests/UnitTests/Domain/Exceptions/DomainExceptionTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using GymPortal.Domain.Exceptions;
+using Xunit;
+
+namespace GymPortal.Tests.UnitTests.Domain.Exceptions
+{
+    public class DomainExceptionTests
+    {
+        [Fact]
+        public void DuplicateBookingException_ShouldHaveCorrectMessage()
+        {
+            // Arrange & Act
+            var exception = new DuplicateBookingException();
+
+            // Assert
+            exception.Message.Should().Be("You have already booked this class.");
+        }
+
+        [Fact]
+        public void ClassFullException_ShouldHaveCorrectMessage()
+        {
+            // Arrange & Act
+            var exception = new ClassFullException();
+
+            // Assert
+            exception.Message.Should().Be("This class is already full.");
+        }
+
+        [Fact]
+        public void DomainException_ShouldBeAbstract()
+        {
+            // Assert
+            typeof(DomainException).IsAbstract.Should().BeTrue();
+        }
+    }
+}
