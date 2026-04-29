f8c9860a81e43c567d19d6b7f5050b7aaa2628f6
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-29 21:10:20 +0200

    refactor(web): rename UserBookedSessionId property
    Renamed UserBookedSessionId to UserBookedSessionIds in ClassesIndexViewModel to accurately represent that it contains multiple session IDs. No functional changes were made.

--- GymPortal.Web/ViewModels/ClassesIndexViewModel.cs
+++ GymPortal.Web/ViewModels/ClassesIndexViewModel.cs
@@ -7,6 +7,6 @@ namespace GymPortal.Web.ViewModels
         public List<ClassSessionDto> Sessions { get; set; } = new();
         public List<string> Categories { get; set; } = new();
         public string SelectedCategory { get; set; } = string.Empty;
-        public HashSet<int> UserBookedSessionId { get; set; } = new();
+        public HashSet<int> UserBookedSessionIds { get; set; } = new();
     }
 }   
