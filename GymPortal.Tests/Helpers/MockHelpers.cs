c7bfdf729ed1806987d7bb16d3bf08803ff3820f
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-04-29 21:04:54 +0200

    feat(tests): add async mock DbSet support for EF Core
    Added BuildMockDbSet extension and supporting async query classes to enable mocking of EF Core DbSet with async LINQ operations in unit tests.

--- GymPortal.Tests/Helpers/MockHelpers.cs
+++ GymPortal.Tests/Helpers/MockHelpers.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using System.Linq.Expressions;
 using System.Security.Claims;
 
 namespace GymPortal.Tests.Helpers
@@ -40,5 +43,68 @@ namespace GymPortal.Tests.Helpers
                 HttpContext = new DefaultHttpContext { User = principal }
             };
         }
+
+        /// <summary>
+        /// Creates a mock DbSet that supports async operations (ToListAsync, FirstOrDefaultAsync, etc.)
+        /// </summary>
+        public static Mock<DbSet<T>> BuildMockDbSet<T>(this IQueryable<T> data) where T : class
+        {
+            var mockSet = new Mock<DbSet<T>>();
+
+            mockSet.As<IAsyncEnumerable<T>>()
+                .Setup(m => m.GetAsyncEnumerator(It.IsAny<CancellationToken>()))
+                .Returns(new TestAsyncEnumerator<T>(data.GetEnumerator()));
+
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestAsyncQueryProvider<T>(data.Provider));
+
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+
+    // Async queryable support classes
+    public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestAsyncEnumerator(IEnumerator<T> inner) => _inner = inner;
+        public T Current => _inner.Current;
+        public ValueTask<bool> MoveNextAsync() => ValueTask.FromResult(_inner.MoveNext());
+        public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+    }
+
+    public class TestAsyncQueryProvider<T> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        public TestAsyncQueryProvider(IQueryProvider inner) => _inner = inner;
+
+        public IQueryable CreateQuery(Expression expression) => new TestAsyncEnumerable<T>(expression);
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression) => new TestAsyncEnumerable<TElement>(expression);
+        public object Execute(Expression expression) => _inner.Execute(expression);
+        public TResult Execute<TResult>(Expression expression) => _inner.Execute<TResult>(expression);
+
+        public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            var result = _inner.Execute(expression);
+            return (TResult)typeof(Task).GetMethod(nameof(Task.FromResult))?.MakeGenericMethod(typeof(TResult))
+                .Invoke(null, new[] { result })!;
+        }
+    }
+
+    public class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
+        public TestAsyncEnumerable(Expression expression) : base(expression) { }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+            => new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+
+        IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
     }
 }
